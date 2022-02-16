e1c72f0bcee3d0fea7291c62ffb7df5436f8a4a5
linhao <823040382@qq.com>
2022-02-16 23:11:07 +0800

    contract code compare

--- collection/StarBlockCompare.sol
+++ collection/StarBlockCompare.sol
@@ -559,7 +559,7 @@ contract ERC721A is
   uint256 private currentIndex = 0;
 
   uint256 internal immutable collectionSize;
-  uint256 internal immutable maxBatchSize;
+  uint256 internal maxBatchSize;
 
   // Token name
   string private _name;
@@ -1164,7 +1164,7 @@ abstract contract Ownable is Context {
     /**
      * @dev Throws if called by any account other than the owner.
      */
-    modifier onlyOwner() {
+    modifier onlyOwner() virtual {
         require(owner() == _msgSender(), "Ownable: caller is not the owner");
         _;
     }
@@ -1196,223 +1196,130 @@ abstract contract Ownable is Context {
     }
 }
 
-contract Azuki is Ownable, ERC721A, ReentrancyGuard {
-  uint256 public immutable maxPerAddressDuringMint;
-  uint256 public immutable amountForDevs;
-  uint256 public immutable amountForAuctionAndDev;
+contract OwnableDelegateProxy {}
 
-  struct SaleConfig {
-    uint32 auctionSaleStartTime;
-    uint32 publicSaleStartTime;
-    uint64 mintlistPrice;
-    uint64 publicPrice;
-    uint32 publicSaleKey;
-  }
+/**
+ * Used to delegate ownership of a contract to another address, to save on unneeded transactions to approve contract use for users
+ */
+contract ProxyRegistry {
+    mapping(address => OwnableDelegateProxy) public proxies;
+}
 
-  SaleConfig public saleConfig;
+contract StarBlock is Ownable, ERC721A, ReentrancyGuard {
 
-  mapping(address => uint256) public allowlist;
-
-  constructor(
-    uint256 maxBatchSize_,
-    uint256 collectionSize_,
-    uint256 amountForAuctionAndDev_,
-    uint256 amountForDevs_
-  ) ERC721A("Azuki", "AZUKI", maxBatchSize_, collectionSize_) {
-    maxPerAddressDuringMint = maxBatchSize_;
-    amountForAuctionAndDev = amountForAuctionAndDev_;
-    amountForDevs = amountForDevs_;
-    require(
-      amountForAuctionAndDev_ <= collectionSize_,
-      "larger collection size needed"
-    );
-  }
-
-  modifier callerIsUser() {
-    require(tx.origin == msg.sender, "The caller is another contract");
-    _;
-  }
-
-  function auctionMint(uint256 quantity) external payable callerIsUser {
-    uint256 _saleStartTime = uint256(saleConfig.auctionSaleStartTime);
-    require(
-      _saleStartTime != 0 && block.timestamp >= _saleStartTime,
-      "sale has not started yet"
-    );
-    require(
-      totalSupply() + quantity <= amountForAuctionAndDev,
-      "not enough remaining reserved for auction to support desired mint amount"
-    );
-    require(
-      numberMinted(msg.sender) + quantity <= maxPerAddressDuringMint,
-      "can not mint this many"
-    );
-    uint256 totalCost = getAuctionPrice(_saleStartTime) * quantity;
-    _safeMint(msg.sender, quantity);
-    refundIfOver(totalCost);
-  }
-
-  function allowlistMint() external payable callerIsUser {
-    uint256 price = uint256(saleConfig.mintlistPrice);
-    require(price != 0, "allowlist sale has not begun yet");
-    require(allowlist[msg.sender] > 0, "not eligible for allowlist mint");
-    require(totalSupply() + 1 <= collectionSize, "reached max supply");
-    allowlist[msg.sender]--;
-    _safeMint(msg.sender, 1);
-    refundIfOver(price);
-  }
-
-  function publicSaleMint(uint256 quantity, uint256 callerPublicSaleKey)
-    external
-    payable
-    callerIsUser
-  {
-    SaleConfig memory config = saleConfig;
-    uint256 publicSaleKey = uint256(config.publicSaleKey);
-    uint256 publicPrice = uint256(config.publicPrice);
-    uint256 publicSaleStartTime = uint256(config.publicSaleStartTime);
-    require(
-      publicSaleKey == callerPublicSaleKey,
-      "called with incorrect public sale key"
-    );
-
-    require(
-      isPublicSaleOn(publicPrice, publicSaleKey, publicSaleStartTime),
-      "public sale has not begun yet"
-    );
-    require(totalSupply() + quantity <= collectionSize, "reached max supply");
-    require(
-      numberMinted(msg.sender) + quantity <= maxPerAddressDuringMint,
-      "can not mint this many"
-    );
-    _safeMint(msg.sender, quantity);
-    refundIfOver(publicPrice * quantity);
-  }
-
-  function refundIfOver(uint256 price) private {
-    require(msg.value >= price, "Need to send more ETH.");
-    if (msg.value > price) {
-      payable(msg.sender).transfer(msg.value - price);
-    }
-  }
-
-  function isPublicSaleOn(
-    uint256 publicPriceWei,
-    uint256 publicSaleKey,
-    uint256 publicSaleStartTime
-  ) public view returns (bool) {
-    return
-      publicPriceWei != 0 &&
-      publicSaleKey != 0 &&
-      block.timestamp >= publicSaleStartTime;
-  }
-
-  uint256 public constant AUCTION_START_PRICE = 1 ether;
-  uint256 public constant AUCTION_END_PRICE = 0.15 ether;
-  uint256 public constant AUCTION_PRICE_CURVE_LENGTH = 340 minutes;
-  uint256 public constant AUCTION_DROP_INTERVAL = 20 minutes;
-  uint256 public constant AUCTION_DROP_PER_STEP =
-    (AUCTION_START_PRICE - AUCTION_END_PRICE) /
-      (AUCTION_PRICE_CURVE_LENGTH / AUCTION_DROP_INTERVAL);
-
-  function getAuctionPrice(uint256 _saleStartTime)
-    public
-    view
-    returns (uint256)
-  {
-    if (block.timestamp < _saleStartTime) {
-      return AUCTION_START_PRICE;
-    }
-    if (block.timestamp - _saleStartTime >= AUCTION_PRICE_CURVE_LENGTH) {
-      return AUCTION_END_PRICE;
-    } else {
-      uint256 steps = (block.timestamp - _saleStartTime) /
-        AUCTION_DROP_INTERVAL;
-      return AUCTION_START_PRICE - (steps * AUCTION_DROP_PER_STEP);
-    }
-  }
-
-  function endAuctionAndSetupNonAuctionSaleInfo(
-    uint64 mintlistPriceWei,
-    uint64 publicPriceWei,
-    uint32 publicSaleStartTime
-  ) external onlyOwner {
-    saleConfig = SaleConfig(
-      0,
-      publicSaleStartTime,
-      mintlistPriceWei,
-      publicPriceWei,
-      saleConfig.publicSaleKey
-    );
-  }
-
-  function setAuctionSaleStartTime(uint32 timestamp) external onlyOwner {
-    saleConfig.auctionSaleStartTime = timestamp;
-  }
-
-  function setPublicSaleKey(uint32 key) external onlyOwner {
-    saleConfig.publicSaleKey = key;
-  }
-
-  function seedAllowlist(address[] memory addresses, uint256[] memory numSlots)
-    external
-    onlyOwner
-  {
-    require(
-      addresses.length == numSlots.length,
-      "addresses does not match numSlots length"
-    );
-    for (uint256 i = 0; i < addresses.length; i++) {
-      allowlist[addresses[i]] = numSlots[i];
-    }
-  }
-
-  // For marketing etc.
-  function devMint(uint256 quantity) external onlyOwner {
-    require(
-      totalSupply() + quantity <= amountForDevs,
-      "too many already minted before dev mint"
-    );
-    require(
-      quantity % maxBatchSize == 0,
-      "can only mint a multiple of the maxBatchSize"
-    );
-    uint256 numChunks = quantity / maxBatchSize;
-    for (uint256 i = 0; i < numChunks; i++) {
-      _safeMint(msg.sender, maxBatchSize);
-    }
-  }
-
-  // // metadata URI
   string private _baseTokenURI;
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return _baseTokenURI;
-  }
+  /* Proxy registry address. */
+  address public proxyRegistryAddress;
 
-  function setBaseURI(string calldata baseURI) external onlyOwner {
-    _baseTokenURI = baseURI;
-  }
+  uint256 public maxPerAddressDuringMint;
 
-  function withdrawMoney() external onlyOwner nonReentrant {
-    (bool success, ) = msg.sender.call{value: address(this).balance}("");
-    require(success, "Transfer failed.");
-  }
+   constructor(
+        string memory _name,
+        string memory _symbol,
+        address _proxyRegistryAddress,
+        uint256 _maxBatchSize,
+        uint256 _collectionSize,
+        uint256 _maxPerAddressDuringMint,
+        string memory baseURI
+    ) ERC721A(_name, _symbol, _maxBatchSize, _collectionSize) {
 
-  function setOwnersExplicit(uint256 quantity) external onlyOwner nonReentrant {
-    _setOwnersExplicit(quantity);
-  }
+        proxyRegistryAddress = _proxyRegistryAddress;
+        maxPerAddressDuringMint = _maxPerAddressDuringMint;
+        if (bytes(baseURI).length > 0) {
+            setBaseURI(baseURI);
+        }
+    }
 
-  function numberMinted(address owner) public view returns (uint256) {
-    return _numberMinted(owner);
-  }
+       /**
+     * @dev Throws if called by any account other than the owner or their proxy
+     */
+    modifier onlyOwner() override {
+        require(
+            _isOwner(_msgSender()),
+            "StarBlockAsset#onlyOwner: CALLER_IS_NOT_OWNER"
+        );
+        _;
+    }
 
-  function getOwnershipData(uint256 tokenId)
-    external
-    view
-    returns (TokenOwnership memory)
-  {
-    return ownershipOf(tokenId);
-  }
+     /**
+     * Override isApprovedForAll to whitelist user proxy accounts to enable gas-less listings.
+     */
+    function isApprovedForAll(address owner, address operator)
+        override
+        public
+        view
+        returns (bool)
+    {
+        // Whitelist proxy contracts for easy trading.
+        if (_isProxyForUser(owner, operator)) {
+            return true;
+        }
+        return super.isApprovedForAll(owner, operator);
+    }
+
+    function _isOwner(address _address) internal view returns (bool) {
+        return owner() == _address || _isProxyForUser(owner(), _address);
+    }
+
+    // PROXY HELPER METHODS
+    function _isProxyForUser(address _user, address _address)
+        internal
+        view
+        returns (bool)
+    {
+        return _proxy(_user) == _address;
+    }
+
+    function _proxy(address _address) internal view returns (address) {
+        ProxyRegistry proxyRegistry = ProxyRegistry(proxyRegistryAddress);
+        return address(proxyRegistry.proxies(_address));
+    }
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return _baseTokenURI;
+    }
+
+    function setBaseURI(string memory baseURI) public onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function setMaxPerAddressDuringMint(uint256 _maxPerAddressDuringMint) public onlyOwner {
+        maxPerAddressDuringMint = _maxPerAddressDuringMint;
+    }
+
+    function setMaxBatchSize(uint256 _maxBatchSize) public onlyOwner {
+        maxBatchSize = _maxBatchSize;
+    }
+
+    function mintAssets(
+        address _to,
+        uint256 quantity
+    ) public onlyOwner {
+
+     require(totalSupply() + quantity <= collectionSize, "StarBlockAsset#reached max supply");
+        _safeMint(_to, quantity);
+
+     require(
+      numberMinted(_to) + quantity <= maxPerAddressDuringMint,
+       "StarBlockAsset#can not mint this many"
+       );
+
+       _safeMint(_to, quantity);
+    }
+
+     function collectionMaxSize() public view returns (uint256) {
+       return collectionSize;
+     }
+
+    function withdrawMoney() external onlyOwner nonReentrant {
+      (bool success, ) = msg.sender.call{value: address(this).balance}("");
+      require(success, "StarBlockAsset#Transfer failed.");
+    }
+
+    function numberMinted(address owner) public view returns (uint256) {
+     return _numberMinted(owner);
+    }
+
 }
 
--- newExchange/StarBlockExchangeCompare.sol
+++ newExchange/StarBlockExchangeCompare.sol
@@ -4,6 +4,7 @@
 
 pragma solidity ^0.4.13;
 
+
 library SafeMath {
 
   /**
@@ -363,6 +364,9 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
     /* Token transfer proxy. */
     TokenTransferProxy public tokenTransferProxy;
 
+    /* CollectionSell. */
+    CollectionSellStrategy public collectionSellStrategy;
+
     /* Cancelled / finalized orders, by hash. */
     mapping(bytes32 => bool) public cancelledOrFinalized;
 
@@ -449,6 +453,11 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
     event OrderCancelled          (bytes32 indexed hash);
     event OrdersMatched           (bytes32 buyHash, bytes32 sellHash, address indexed maker, address indexed taker, uint price, bytes32 indexed metadata);
 
+
+    function withdrawMoney() external onlyOwner reentrancyGuard {
+       (bool success, ) = msg.sender.call.value(address(this).balance)("");
+       require(success, "ExchangeCore#Transfer failed.");
+    }
     /**
      * @dev Change the minimum maker fee paid to the protocol (owner only)
      * @param newMinimumMakerProtocolFee New fee to set in basis points
@@ -480,6 +489,28 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
         onlyOwner
     {
         protocolFeeRecipient = newProtocolFeeRecipient;
+    }
+
+    /**
+     * @dev Change CollectionSellStrategy (owner only)
+     * @param newCollectionSellStrategy New CollectionSell
+     */
+    function changeCollectionSellStrategy(CollectionSellStrategy newCollectionSellStrategy)
+        public
+        onlyOwner
+    {
+        collectionSellStrategy = newCollectionSellStrategy;
+    }
+
+      /**
+     * @dev Change exchangeToken (owner only)
+     * @param newExchangeToken New exchangeToken
+     */
+    function changeExchangeToken(ERC20 newExchangeToken)
+        public
+        onlyOwner
+    {
+        exchangeToken = newExchangeToken;
     }
 
     /**
@@ -1028,15 +1059,24 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
         if (msg.sender != buy.maker) {
             cancelledOrFinalized[buyHash] = true;
         }
-        if (msg.sender != sell.maker) {
+
+        if (msg.sender != sell.maker && sell.saleKind != SaleKindInterface.SaleKind.CollectionSell) {
             cancelledOrFinalized[sellHash] = true;
         }
 
+        uint256 quantity = getQuantity(buy, sell);
         /* INTERACTIONS */
+        /* change the baseprice based on the qutity. */
+        if (sell.saleKind == SaleKindInterface.SaleKind.CollectionSell) {
+            updateTotalPrice(buy, sell);
 
+          /* collectionSell canBuy ckeck. */ 
+          require(collectionSellStrategy.canBuyCollection(buy.maker, sell.maker, sell.target, quantity));
+        }
+        
         /* Execute funds transfer and pay fees. */
         uint price = executeFundsTransfer(buy, sell);
-
+        
         /* Execute specified call through proxy. */
         require(proxy.proxy(sell.target, sell.howToCall, sell.calldata));
 
@@ -1052,10 +1092,37 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
             require(staticCall(sell.staticTarget, sell.calldata, sell.staticExtradata));
         }
 
+         /* collectionSell afterBuy ckeck. */ 
+        collectionSellStrategy.afterBuyCollection(buy.maker, sell.maker, sell.target, quantity);
+
         /* Log match event. */
         emit OrdersMatched(buyHash, sellHash, sell.feeRecipient != address(0) ? sell.maker : buy.maker, sell.feeRecipient != address(0) ? buy.maker : sell.maker, price, metadata);
     }
 
+    function updateTotalPrice(Order memory buy, Order memory sell) internal pure {
+
+            uint256 quatity = getQuantity(buy, sell);
+            sell.basePrice = sell.basePrice * quatity;
+            buy.basePrice = sell.basePrice;
+    }
+
+    function getQuantity(Order memory buy, Order memory sell) internal pure returns (uint256) {
+
+            bytes memory quantityBytes = new bytes(2);
+            quantityBytes[0] = buy.calldata[buy.calldata.length - 2];
+            quantityBytes[1] = buy.calldata[buy.calldata.length - 1];
+            uint256 quantity = bytesToUint(quantityBytes);
+            return quantity;
+     }
+
+
+    function bytesToUint(bytes memory b) internal pure returns (uint256) {
+        uint256 number;
+        for(uint i = 0; i< b.length; i++){
+            number = number + uint8(b[i])*(2**(8*(b.length-(i+1))));
+        }
+        return  number;
+    }
 }
 
 contract Exchange is ExchangeCore {
@@ -1373,9 +1440,8 @@ contract Exchange is ExchangeCore {
         uint8[2] vs,
         bytes32[5] rssMetadata)
         public
-        payable
+        payable callerIsUser
     {
-
         return atomicMatch(
           Order(addrs[0], addrs[1], addrs[2], uints[0], uints[1], uints[2], uints[3], addrs[3], FeeMethod(feeMethodsSidesKindsHowToCalls[0]), SaleKindInterface.Side(feeMethodsSidesKindsHowToCalls[1]), SaleKindInterface.SaleKind(feeMethodsSidesKindsHowToCalls[2]), addrs[4], AuthenticatedProxy.HowToCall(feeMethodsSidesKindsHowToCalls[3]), calldataBuy, replacementPatternBuy, addrs[5], staticExtradataBuy, ERC20(addrs[6]), uints[4], uints[5], uints[6], uints[7], uints[8]),
           Sig(vs[0], rssMetadata[0], rssMetadata[1]),
@@ -1385,13 +1451,18 @@ contract Exchange is ExchangeCore {
         );
     }
 
+    modifier callerIsUser() {
+     require(tx.origin == msg.sender, "The caller is another contract");
+    _;
+  }
+
 }
 
-contract WyvernExchange is Exchange {
+contract StarBlockExchange is Exchange {
 
-    string public constant name = "Project Wyvern Exchange";
+    string public constant name = "Project StarBlock Exchange";
 
-    string public constant version = "2.2";
+    string public constant version = "1.0";
 
     string public constant codename = "Lambton Worm";
 
@@ -1400,14 +1471,31 @@ contract WyvernExchange is Exchange {
      * @param registryAddress Address of the registry instance which this Exchange instance will use
      * @param tokenAddress Address of the token used for protocol fees
      */
-    constructor (ProxyRegistry registryAddress, TokenTransferProxy tokenTransferProxyAddress, ERC20 tokenAddress, address protocolFeeAddress) public {
+    constructor (ProxyRegistry registryAddress, TokenTransferProxy tokenTransferProxyAddress, ERC20 tokenAddress, address protocolFeeAddress, CollectionSellStrategy _collectionSellStrategy) public {
         registry = registryAddress;
         tokenTransferProxy = tokenTransferProxyAddress;
         exchangeToken = tokenAddress;
         protocolFeeRecipient = protocolFeeAddress;
+        collectionSellStrategy = _collectionSellStrategy;
         owner = msg.sender;
     }
 
+}
+
+contract CollectionSellStrategy  {
+    /**
+     * @dev check if user can buy the colleciton assets.
+     */
+    function canBuyCollection(address buy, address sell, address collection, uint256 quantity) external view returns (bool) {
+        return true;
+    }
+    
+    /**
+     * @dev update some info after buy collection
+     */
+    function afterBuyCollection(address buy, address sell, address collection, uint256 quantity) external returns (bool) {
+        return true;
+    }
 }
 
 library SaleKindInterface {
@@ -1422,7 +1510,7 @@ library SaleKindInterface {
      * English auctions cannot be supported without stronger escrow guarantees.
      * Future interesting options: Vickrey auction, nonlinear Dutch auctions.
      */
-    enum SaleKind { FixedPrice, DutchAuction }
+    enum SaleKind { FixedPrice, DutchAuction, CollectionSell}
 
     /**
      * @dev Check whether the parameters of a sale are valid
@@ -1436,7 +1524,16 @@ library SaleKindInterface {
         returns (bool)
     {
         /* Auctions must have a set expiration date. */
-        return (saleKind == SaleKind.FixedPrice || expirationTime > 0);
+        if (expirationTime > 0 ) {
+            return true;
+        }else {
+            if (saleKind == SaleKind.FixedPrice) {
+                return true;
+            }else if (saleKind == SaleKind.CollectionSell) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -1469,6 +1566,8 @@ library SaleKindInterface {
         returns (uint finalPrice)
     {
         if (saleKind == SaleKind.FixedPrice) {
+            return basePrice;
+        }else if (saleKind == SaleKind.CollectionSell) {
             return basePrice;
         } else if (saleKind == SaleKind.DutchAuction) {
             uint diff = SafeMath.div(SafeMath.mul(extra, SafeMath.sub(now, listingTime)), SafeMath.sub(expirationTime, listingTime));
