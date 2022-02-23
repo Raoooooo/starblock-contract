daffbaf3ab1794a92d4c7cfc27a6b145e3623747
linhao <823040382@qq.com>
2022-02-23 14:15:13 +0800

    new and origrinal code compare

--- StarBlockExchangeCompare.sol
+++ StarBlockExchangeCompare.sol
@@ -449,6 +449,11 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
     event OrderCancelled          (bytes32 indexed hash);
     event OrdersMatched           (bytes32 buyHash, bytes32 sellHash, address indexed maker, address indexed taker, uint price, bytes32 indexed metadata);
 
+    function withdrawMoney() external onlyOwner reentrancyGuard {
+       (bool success, ) = msg.sender.call.value(address(this).balance)("");
+       require(success, "ExchangeCore#withdrawMoney Transfer failed.");
+    }
+
     /**
      * @dev Change the minimum maker fee paid to the protocol (owner only)
      * @param newMinimumMakerProtocolFee New fee to set in basis points
@@ -480,6 +485,17 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
         onlyOwner
     {
         protocolFeeRecipient = newProtocolFeeRecipient;
+    }
+
+    /**
+     * @dev Change exchangeToken (owner only)
+     * @param newExchangeToken New exchangeToken
+     */
+    function changeExchangeToken(ERC20 newExchangeToken)
+        public
+        onlyOwner
+    {
+        exchangeToken = newExchangeToken;
     }
 
     /**
@@ -1028,10 +1044,20 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
         if (msg.sender != buy.maker) {
             cancelledOrFinalized[buyHash] = true;
         }
-        if (msg.sender != sell.maker) {
+        if (msg.sender != sell.maker && sell.saleKind != SaleKindInterface.SaleKind.CollectionRandomSale) {
             cancelledOrFinalized[sellHash] = true;
         }
 
+        /* INTERACTIONS */
+        /* change the baseprice based on the qutity. */
+        if (sell.saleKind == SaleKindInterface.SaleKind.CollectionRandomSale) {
+           uint256 quantity = getQuantity(buy, sell);
+            if (quantity > 1) {
+                sell.basePrice = sell.basePrice * quantity;
+                buy.basePrice = sell.basePrice;
+            }
+        }
+        
         /* INTERACTIONS */
 
         /* Execute funds transfer and pay fees. */
@@ -1056,6 +1082,28 @@ contract ExchangeCore is ReentrancyGuarded, Ownable {
         emit OrdersMatched(buyHash, sellHash, sell.feeRecipient != address(0) ? sell.maker : buy.maker, sell.feeRecipient != address(0) ? buy.maker : sell.maker, price, metadata);
     }
 
+    function getQuantity(Order memory buy, Order memory sell) internal view returns (uint256) {
+            bytes memory quantityBytes = new bytes(2);
+            uint index = SafeMath.sub(buy.calldata.length, 2);
+            uint lastIndex = SafeMath.sub(buy.calldata.length, 1);
+            quantityBytes[0] = buy.calldata[index];
+            quantityBytes[1] = buy.calldata[lastIndex];
+            uint256 quantity = bytesToUint(quantityBytes);
+            return quantity;
+     }
+
+
+    function bytesToUint(bytes memory b) internal pure returns (uint256) {
+        uint256 number;
+        for(uint i = 0; i< b.length; i++) {
+            uint index = SafeMath.add(i, 1);
+            uint length = SafeMath.sub(b.length, index);
+            uint offset = 2**SafeMath.mul(8, length);
+            uint offsetCount = SafeMath.mul(uint8(b[i]), offset);
+            number = SafeMath.add(number, offsetCount);
+        }
+        return  number;
+    }
 }
 
 contract Exchange is ExchangeCore {
@@ -1373,9 +1421,8 @@ contract Exchange is ExchangeCore {
         uint8[2] vs,
         bytes32[5] rssMetadata)
         public
-        payable
+        payable callerIsUser
     {
-
         return atomicMatch(
           Order(addrs[0], addrs[1], addrs[2], uints[0], uints[1], uints[2], uints[3], addrs[3], FeeMethod(feeMethodsSidesKindsHowToCalls[0]), SaleKindInterface.Side(feeMethodsSidesKindsHowToCalls[1]), SaleKindInterface.SaleKind(feeMethodsSidesKindsHowToCalls[2]), addrs[4], AuthenticatedProxy.HowToCall(feeMethodsSidesKindsHowToCalls[3]), calldataBuy, replacementPatternBuy, addrs[5], staticExtradataBuy, ERC20(addrs[6]), uints[4], uints[5], uints[6], uints[7], uints[8]),
           Sig(vs[0], rssMetadata[0], rssMetadata[1]),
@@ -1385,13 +1432,40 @@ contract Exchange is ExchangeCore {
         );
     }
 
+    modifier callerIsUser() {
+     require(tx.origin == msg.sender, "The caller is another contract");
+    _;
+    }
+
+    function getQuantity(Order memory buy, Order memory sell) internal view returns (uint256) {
+            bytes memory quantityBytes = new bytes(2);
+            uint index = SafeMath.sub(buy.calldata.length, 2);
+            uint lastIndex = SafeMath.sub(buy.calldata.length, 1);
+            quantityBytes[0] = buy.calldata[index];
+            quantityBytes[1] = buy.calldata[lastIndex];
+            uint256 quantity = bytesToUint(quantityBytes);
+            return quantity;
+     }
+
+
+    function bytesToUint(bytes memory b) internal pure returns (uint256) {
+        uint256 number;
+        for(uint i = 0; i< b.length; i++) {
+            uint index = SafeMath.add(i, 1);
+            uint length = SafeMath.sub(b.length, index);
+            uint offset = 2**SafeMath.mul(8, length);
+            uint offsetCount = SafeMath.mul(uint8(b[i]), offset);
+            number = SafeMath.add(number, offsetCount);
+        }
+        return  number;
+    }
 }
 
-contract WyvernExchange is Exchange {
+contract StarBlockExchange is Exchange {
 
-    string public constant name = "Project Wyvern Exchange";
+    string public constant name = "Project StarBlock Exchange";
 
-    string public constant version = "2.2";
+    string public constant version = "1.0";
 
     string public constant codename = "Lambton Worm";
 
@@ -1422,7 +1496,7 @@ library SaleKindInterface {
      * English auctions cannot be supported without stronger escrow guarantees.
      * Future interesting options: Vickrey auction, nonlinear Dutch auctions.
      */
-    enum SaleKind { FixedPrice, DutchAuction }
+    enum SaleKind { FixedPrice, DutchAuction, CollectionRandomSale}
 
     /**
      * @dev Check whether the parameters of a sale are valid
@@ -1436,7 +1510,16 @@ library SaleKindInterface {
         returns (bool)
     {
         /* Auctions must have a set expiration date. */
-        return (saleKind == SaleKind.FixedPrice || expirationTime > 0);
+        if (expirationTime > 0 ) {
+            return true;
+        }else {
+            if (saleKind == SaleKind.FixedPrice) {
+                return true;
+            }else if (saleKind == SaleKind.CollectionRandomSale) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -1470,7 +1553,9 @@ library SaleKindInterface {
     {
         if (saleKind == SaleKind.FixedPrice) {
             return basePrice;
-        } else if (saleKind == SaleKind.DutchAuction) {
+        }else if (saleKind == SaleKind.CollectionRandomSale) {
+            return basePrice; 
+        }else if (saleKind == SaleKind.DutchAuction) {
             uint diff = SafeMath.div(SafeMath.mul(extra, SafeMath.sub(now, listingTime)), SafeMath.sub(expirationTime, listingTime));
             if (side == Side.Sell) {
                 /* Sell-side - start price: basePrice. End price: basePrice - extra. */
