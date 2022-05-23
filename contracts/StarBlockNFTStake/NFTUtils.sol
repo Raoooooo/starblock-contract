38b941b74a868bbc9b5254c0a9dc9ac6eaf4f4c1
linhao <823040382@qq.com>
2022-05-23 15:42:59 +0800

    nft utils ownedTokens

--- contracts/StarBlockNFTStake/NFTUtils.sol
+++ contracts/StarBlockNFTStake/NFTUtils.sol
@@ -6,18 +6,10 @@ pragma solidity ^0.8.0;
 // import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 // import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-
-interface IWrappedNFT is IERC721Metadata {
-    event SetAdmin(address _admin);
-    event Deposit(address indexed _forUser, uint256[] _tokenIds);
-    event Withdraw(address indexed _forUser, uint256[] _wnftTokenIds);
-
-    function nft() external view returns (IERC721Metadata);
-    function deposit(address _forUser, uint256[] memory _tokenIds) external;
-    function withdraw(address _forUser, uint256[] memory _wnftTokenIds) external;
-}
+import "./wnft_interfaces.sol";
 
 interface INFTMasterChef {
     // Info of each user.
@@ -49,13 +41,10 @@ interface INFTMasterChef {
 
         IERC20 dividendToken;
         uint256 accDividendPerShare;
-
-        // mapping (uint256 => NFTInfo) nfts;
     }
     
-    function poolInfo(uint256 _pid) external view returns (PoolInfo memory);
-    
-    function poolNFTInfo(uint256 _pid, uint256 _nftTokenId) external view returns (NFTInfo memory);
+    function poolInfos(uint256 _pid) external view returns (PoolInfo memory poolInfo);
+    function poolNFTInfos(uint256 _pid, uint256 _nftTokenId) external view returns (bool deposited, uint256 rewardDebt, uint256 dividendDebt);
 
     function pending(uint256 _pid, uint256[] memory _wnftTokenIds) external view returns (uint256 mining, uint256 dividend);
     function deposit(uint256 _pid, uint256[] memory _tokenIds) external payable;
@@ -63,16 +52,17 @@ interface INFTMasterChef {
     function harvest(uint256 _pid, address _to, uint256[] memory _wnftTokenIds) external returns (uint256 mining, uint256 dividend);
 }
   
-contract NFTUtils {
+contract NFTUtils { 
+
     function getNFTMasterChefInfos(INFTMasterChef _nftMasterchef, uint256 _pid, address _owner, uint256 _maxTokenId) public view
      returns (INFTMasterChef.PoolInfo memory poolInfo, uint256 mining, uint256 dividend, uint256 nftQuantity, uint256 wnftQuantity) 
      {
        require(address(_nftMasterchef) != address(0),"NFTUtils: nftMasterchef address is the zero address");
        INFTMasterChef nftMasterchef = _nftMasterchef; 
-       poolInfo = nftMasterchef.poolInfo(_pid);
+       poolInfo = nftMasterchef.poolInfos(_pid);
 
        if (_owner != address(0)) {
-         uint256[] memory wnftTokenIds = ownTokens(poolInfo.wnftContract, _owner, _maxTokenId);
+         uint256[] memory wnftTokenIds = ownedWNFTTokens(poolInfo.wnftContract, _owner, _maxTokenId);
          require(wnftTokenIds.length > 0, "NFTInstrument:owner has no wnftToken");
          (mining,  dividend) = nftMasterchef.pending(_pid, wnftTokenIds);
 
@@ -83,22 +73,41 @@ contract NFTUtils {
        }
     }
     
-    function ownTokens(IERC721 _nftContract, address _owner, uint256 _maxTokenId) public view returns (uint256[] memory totalTokens) {
-         require(address(_nftContract) != address(0) && _owner != address(0) && _maxTokenId > 0,"NFTUtils: nftContract address or owner is the zero address");
-        //  uint256 maxIndex;
-        //  for (uint256 i = 0; i < _quantity; i++) {
-        //        address tokenOwner = _nftContract.ownerOf(i);
-        //       if (tokenOwner == _owner) {
-        //          maxIndex++;
-        //       }
-        //   }
-        //   require(maxIndex > 0, "NFTUtils: owner has no wnftToken");
-          uint256 index;
-          for (uint256 i = 0; i < _maxTokenId; i++) {
-               address tokenOwner = _nftContract.ownerOf(i);
-              if (tokenOwner == _owner) {
-                  totalTokens[index ++] = i;
-              }
+    function ownedWNFTTokens(IWrappedNFT _wnftContract, address _owner, uint256 _maxTokenId) public view returns (uint256[] memory totalTokens) {
+         require(address(_wnftContract) != address(0) && _owner != address(0) && _maxTokenId >= 0, "NFTUtils: wnftContract address or owner is the zero address");
+       
+         uint256 maxIndex;
+          for (uint256 i  = 0; i <= _maxTokenId; i++) {
+             if (_wnftContract.exists(i)) {
+                 address tokenOwner = _wnftContract.ownerOf(i);
+                 if (tokenOwner == _owner) {
+                     maxIndex++;
+                 }
+             }
+         }
+         require(maxIndex > 0, "NFTUtils: owner has no token");
+         totalTokens = new uint256[](maxIndex);
+         uint256 index;
+         for (uint256 i  = 0; i <= _maxTokenId; i++) {
+             if (_wnftContract.exists(i)) {
+                 address tokenOwner = _wnftContract.ownerOf(i);
+                 if (tokenOwner == _owner) {
+                     totalTokens[index] = i;
+                     index++;
+                 }
+             }
+         }
+    }
+
+    function ownedTokens(IERC721Enumerable _nftContract, address _owner) public view returns (uint256[] memory totalTokens) {
+         require(address(_nftContract) != address(0) && _owner != address(0), "NFTUtils: nftContract address or owner is the zero address");
+         uint256 balance = _nftContract.balanceOf(_owner);
+         require(balance > 0, "NFTUtils: owner has no token");
+         totalTokens = new uint256[](balance);
+          for (uint256 i = 0; i < balance; i++) {
+              uint256 tokenId = _nftContract.tokenOfOwnerByIndex(_owner, i);
+              totalTokens[i] = tokenId;
           }
     }
+
 }
